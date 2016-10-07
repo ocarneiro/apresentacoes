2c881b1eac939ac67bcd47781262a6c326101afc
Otavio Carneiro <otavio@carneiro.blog.br>
2016-10-07 01:02:04 -0300

    Reorganização do código usado na palestra

--- python/pybr12/code/tmp/demo4.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-video = cv2.VideoCapture('video2_480.mp4')
-retorno, imagem = video.read()
-
-while retorno:
-    cv2.imshow("cv", imagem)
-    _ = cv2.waitKey(10)
-    retorno, imagem = video.read()
